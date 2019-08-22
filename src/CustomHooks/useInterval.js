de928d27d90fb8f9e263c8f73aeebcb5e752fc69
Murray918 <darbyguitars@gmail.com>
2019-08-21 19:39:07 -0500

    adding some comments for later use

--- src/CustomHooks/useInterval.js
+++ src/CustomHooks/useInterval.js
@@ -1,23 +1,26 @@
 import { useEffect, useRef } from 'react'
 
+/*
+ * *this is a hook that was designed by none other than Dan Abramov
+ */
+
 export default (callback, delay) => {
-    const savedCallback = useRef()
-    // this is to remember the current callback
-    useEffect(() => {
-        savedCallback.current = callback
-    },[callback])
+	const savedCallback = useRef()
+	// this is to remember the current callback
+	useEffect(() => {
+		savedCallback.current = callback
+	}, [callback])
 
-    // set up the interval
-    useEffect(() => {
-        function tick() {
-            savedCallback.current()
-        }
-        if(delay !== null) {
-            const id = setInterval(tick, delay)
-            return () => {
-                clearInterval(id)
-            }
-        }
-    }, [delay])
-
+	// set up the interval
+	useEffect(() => {
+		function tick() {
+			savedCallback.current()
+		}
+		if (delay !== null) {
+			const id = setInterval(tick, delay)
+			return () => {
+				clearInterval(id)
+			}
+		}
+	}, [delay])
 }
