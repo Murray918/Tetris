c97f75323a02212221e32f522224655b81169429
Murray918 <darbyguitars@gmail.com>
2019-09-25 22:24:28 -0500

    added the styled component	modified:   src/components/StartButton.js created styled component	new file:   src/components/styles/StyledStartButton.js

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import { StyledStartButton } from './styles/StyledStartButton'
+import PropTypes from 'prop-types'
 
-export default ({ callback }) => {
-	return <div>Start</div>
+const StartButton = ({ callback }) => {
+	return <StyledStartButton onClick={callback}>Start</StyledStartButton>
+}
+
+export default StartButton
+
+StartButton.propTypes = {
+	callback : PropTypes.func
 }
