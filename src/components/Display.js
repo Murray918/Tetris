6fe4bb9e78b4fd4ca6b6ba5e68654d9c54936a2e
Murray918 <darbyguitars@gmail.com>
2019-09-25 22:30:42 -0500

    changed props to type	modified:   src/components/Cell.js added prop types	modified:   src/components/Display.js

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { StyledDisplay } from './styles/StyledDisplay'
+import PropTypes from 'prop-types'
 
-export default ({ gameOver, text }) => {
+const Display = ({ gameOver, text }) => {
 	return <StyledDisplay gameOver={gameOver}>{text}</StyledDisplay>
+}
+
+export default Display
+
+Display.propTypes = {
+	gameOver: PropTypes.bool,
+	text: PropTypes.string
 }
