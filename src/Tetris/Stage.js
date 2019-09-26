8f7810687bea5db83663eee519731f8d1c15b123
Murray918 <darbyguitars@gmail.com>
2019-09-25 21:34:03 -0500

    added the proper props to the stage so that our styling will come through as we want 	modified:   src/Tetris/Stage.js

--- src/Tetris/Stage.js
+++ src/Tetris/Stage.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Cell } from '../components'
 import PropTypes from 'prop-types'
+import { StyledStage } from '../components/styles/StyledStage'
 
 const Stage = ({ stage }) => {
 	const composedStage = stage.map(row =>
 		row.map((cell, x) => <Cell key={x} type={cell[0]} />)
 	)
 
-	return <div>{composedStage}</div>
+	return (
+		<StyledStage width={stage[0].length} height={stage.length}>
+			{composedStage}
+		</StyledStage>
+	)
 }
 
 export default Stage
 
 Stage.propTypes = {
-	stage: PropTypes
+	stage: PropTypes.array
 }
