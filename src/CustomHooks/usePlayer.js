8e6979e7b13ec7a7bccfa652955b0afec3294610
Andrew Murray <darbyguitars@gmail.com>
2019-10-28 16:10:59 -0500

    Tetris.js => had to add the resetPlayer to the useStage hook for player to be defined useplayer.js => had to change randomTetromino() to randomTetromino().shape in line 8

--- src/CustomHooks/usePlayer.js
+++ src/CustomHooks/usePlayer.js
@@ -1,11 +1,11 @@
 import { useState, useCallback } from 'react'
 import { STAGE_WIDTH } from '../gameHelpers'
-import { randomTetromino } from '../tetrominos'
+import { TETROMINOS, randomTetromino } from '../tetrominos'
 
 export const usePlayer = () => {
 	const [player, setPlayer] = useState({
 		pos: { x: 0, y: 0 },
-		tetromino: randomTetromino(),
+		tetromino: randomTetromino().shape,
 		collided: false
 	})
 
