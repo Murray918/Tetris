527129ba065d27459f513a8b644d4812063e682d
Andrew Murray <darbyguitars@gmail.com>
2019-10-28 20:32:58 -0500

    used our collision detections in our move player and drop functions set up a basic game over

--- src/Tetris/Tetris.js
+++ src/Tetris/Tetris.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { createStage } from '../gameHelpers'
+import { createStage, checkCollision } from '../gameHelpers'
 import Stage from './Stage'
 import { useStage } from '../CustomHooks/useStage'
 
@@ -9,6 +9,7 @@ import {
 	StyledTetris
 } from '../components/styles/StyledTetris'
 import { usePlayer } from '../CustomHooks/usePlayer'
+import { platform } from 'os'
 
 const Tetris = () => {
 	// set up our state
@@ -23,16 +24,29 @@ const Tetris = () => {
 	console.log('re-render')
 
 	const movePlayer = dir => {
-		updatePlayerPos({x: dir, y:0})
+		if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+			updatePlayerPos({ x: dir, y: 0 })
+		}
 	}
 
 	const startGame = () => {
 		setStage(createStage())
 		resetPlayer()
+		setGameOver(false)
 	}
 
 	const drop = () => {
-		updatePlayerPos({x: 0, y: 1, collided : false})
+		if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+			updatePlayerPos({ x: 0, y: 1, collided: false })
+		} else {
+			// Game Over
+			if (player.pos.y < 1) {
+				console.log('GAME OVER')
+				setGameOver(true)
+				setDropTime(null)
+			}
+			updatePlayerPos({ x: 0, y: 0, collided: true })
+		}
 	}
 
 	const dropPlayer = () => {
@@ -40,24 +54,24 @@ const Tetris = () => {
 	}
 
 	const move = ({ keyCode }) => {
-		if(!gameOver) {
+		if (!gameOver) {
 			switch (keyCode) {
 				case 37:
 					movePlayer(-1)
-					break;
+					break
 				case 39:
 					movePlayer(1)
-					break;
+					break
 				case 40:
 					dropPlayer()
 				default:
-					break;
+					break
 			}
 		}
 	}
 
 	return (
-		<StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)} >
+		<StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
 			<StyledTetris>
 				<Stage stage={stage} />
 				<div>
