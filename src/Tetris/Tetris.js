73b8da0865cbe12802329d0ed8cf335052a2dc5d
Andrew Murray <darbyguitars@gmail.com>
2019-10-28 19:25:44 -0500

    change the onClick prop to callback in the for the StartButton component 	modified:   src/Tetris/Tetris.js

--- src/Tetris/Tetris.js
+++ src/Tetris/Tetris.js
@@ -71,7 +71,7 @@ const Tetris = () => {
 								<Display text="Level1" />
 							</div>
 						)}
-						<StartButton onClick={startGame} />
+						<StartButton callback={startGame} />
 					</aside>
 				</div>
 			</StyledTetris>
