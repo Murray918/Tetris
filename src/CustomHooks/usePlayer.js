e06d593f8aded2013c730ef755930c389802c509
Andrew Murray <darbyguitars@gmail.com>
2019-10-28 21:25:27 -0500

    the function added checks the side movements of the tetromino as it is falling and makes sure it cannot penetrate and surface that should trigger collisiony

--- src/CustomHooks/usePlayer.js
+++ src/CustomHooks/usePlayer.js
@@ -1,5 +1,5 @@
 import { useState, useCallback } from 'react'
-import { STAGE_WIDTH } from '../gameHelpers'
+import { STAGE_WIDTH, checkCollision } from '../gameHelpers'
 import { TETROMINOS, randomTetromino } from '../tetrominos'
 
 export const usePlayer = () => {
@@ -23,6 +23,19 @@ export const usePlayer = () => {
 		// this method of cloning only works because our object fits the JSON syntax
 		const clonedPlayer = JSON.parse(JSON.stringify(player))
 		clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir)
+
+		const pos = clonedPlayer.pos.x
+		let offset = 1
+		while(checkCollision(clonedPlayer, stage, {x:0, y:0})){
+			clonedPlayer.pos.x += offset
+			offset = -(offset + (offset > 0 ? 1 : -1))
+			if(offset > clonedPlayer.tetromino[0].length){
+				rotate(clonedPlayer.tetromino, -dir)
+				clonedPlayer.pos.x = pos
+				return
+			}
+		}
+
 		setPlayer(clonedPlayer)
 	}
 
